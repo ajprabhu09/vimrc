41ef14139c6d729e48510a1deefd25080e535751
ajprabhu09 <ajinkyaprabhu97@gmail.com>
2023-03-05 10:55:46 -0500

    added color scheme

---
+++ after/plugin/colors.lua
@@ -0,0 +1,4 @@
+
+vim.cmd.colorscheme("tokyonight-night")
+
+-- vim api has some functions to define translucency
--- lua/amprabhu/packer.lua
+++ lua/amprabhu/packer.lua
@@ -14,6 +14,6 @@ return require('packer').startup(function(use)
 	  -- or                            , branch = '0.1.x',
 	  requires = { {'nvim-lua/plenary.nvim'} }
   }
-
+  use 'folke/tokyonight.nvim'
 
 end)
